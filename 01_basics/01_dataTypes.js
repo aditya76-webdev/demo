91ff178128692952ccedda0184367b5599453539
aditya76-webdev <adityajain77356@gmail.com>
2023-11-14 13:10:35 +0530

    dataTypes added

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -1,6 +1,19 @@
 "use strict";
 
-alert('hello');
+// alert('hello');
 
-console.log('aditya');
+console.log('aditya'); 
+
+// alert(3+3);// no popup becoz we are using nodejs not browser
+
+let name = 'aditya';
+let age = 18;
+let isLoggedIn = true ;
+
+// null => standalone value
+
+//  undefined => value not assigned yet
+
+console.log(typeof 'aditya');
+console.log(typeof age);
 
