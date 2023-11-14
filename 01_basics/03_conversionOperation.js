27837a3059a158af9a55d6825d2c6e8b25e6b51a
aditya76-webdev <adityajain77356@gmail.com>
2023-11-14 15:15:46 +0530

    conversionOperation added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(  valueInNumber);
+
+// let boolean = 0;
+
+// let valueOfBoolean = Boolean(boolean);
+
+// console.log( valueOfBoolean);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log( someNumber);
