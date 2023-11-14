77b2f85ddcbbd54b93f5378024ad96f2771a39a0
aditya76-webdev <adityajain77356@gmail.com>
2023-11-14 12:21:44 +0530

    added variables.js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 975526;
+let accountEmail = 'adityajain77356@gmail.com';
+var accountPassword = '12345';
+accountCity = 'Jaipur';
+let accountState;
+
+
+// accountId = '23'; //not allowed
+
+console.log(accountId);
+
+accountEmail = 'hc@hc.com';
+
+accountPassword = '4598';
+
+accountCity = 'Indore';
+
+console.table([accountId , accountEmail , accountPassword , accountCity, accountState  ]);
+
+// {} scope 
